9a9d8b893fe2b753250b8b7de46a8d0407341e9e
9001381 <klesio.silva.frwk@araujo.com.br>
2024-03-21 20:52:58 -0300

    teste comment

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-07987
+# SPDX-License-Identifier: MIT-07987bmbm
 
 from web import myapp
 import unittest
