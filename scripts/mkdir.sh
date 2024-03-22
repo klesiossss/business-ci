49f965cd8830c1a551ae3eddc4a97672859b3ddd
9001381 <klesio.silva.frwk@araujo.com.br>
2024-03-22 03:35:44 -0300

    teste comment

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Create directory
 mkdir -p /web
+
+# Download get-pip.py
 curl -O https://bootstrap.pypa.io/get-pip.py
+
+# Install pip for Python 3.8
 python3.8 get-pip.py --user
-python3.8 -m pip install Flask
+
+# Install Flask using pip for Python 3.8
+python3.8 -m pip install Flask --user
